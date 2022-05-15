61b3f981816866d0afee1e8b640130a4a0e1bc88
Sangho Kim <ksh51999@naver.com>
2022-05-15 11:36:03 +0900

    CI potfolio 게시판 - mod2

--- ci_portfolio_board/views/portfolio/header_v.php
+++ ci_portfolio_board/views/portfolio/header_v.php
@@ -21,10 +21,8 @@
 		<header>
 			<div class="h_top">
 				<ul>
-					<li class="login"> <a href="#">로그인</a>
-					</li>
-					<li class="sitemap"> <a href="">사이트맵</a>
-					</li>
+					<li class="login"> <a href="#">로그인</a></li>
+					<li class="sitemap"> <a href="">사이트맵</a></li>
 				</ul>
 			</div>
 			<div class="h_title"><b><a href="/portfolio/main">포 트 폴 리 오</a></b></div>
@@ -34,21 +32,16 @@
 		<nav class="top_nav">
 			<div>
 			<ul>
-				<li class="webProgramming"> <a href="/portfolio/main/shboard/Pf/list">포트폴리오</a>
-				</li>
-				<li class="board"> <a href="/portfolio/main/shboard/Free/list">게시판</a>
-				</li>
-				<li class="about"> <a href="/portfolio/main/about">ABOUT</a>
-				</li>
+				<li class="webProgramming"> <a href="/portfolio/main/shboard/Pf/list">포트폴리오</a></li>
+				<li class="board"> <a href="/portfolio/main/shboard/Free/list">게시판</a></li>
+				<li class="about"> <a href="/portfolio/main/about">ABOUT</a></li>
 			</ul>
 			</div>
-			<div class="t_nav_path"> HOME > 폴트폴리오
-			</div>
+			<div class="t_nav_path"> HOME > 폴트폴리오</div>
 		</nav>
 		<!-- 왼쪽 메뉴 -->
 		<nav class="side_nav">
 			<ul>
-				<li> <a href="#">웹프로그래밍</a>
-				</li>
+				<li> <a href="#">웹프로그래밍</a></li>
 			</ul>
 		</nav>
